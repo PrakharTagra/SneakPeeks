4bb899d464c45039a4a3cf0de5b2a886ab267722
PrakharTagra <prakhartagra16@gmail.com>
2025-12-27 01:39:58 +0530

    hi

--- app.js
+++ app.js
@@ -32,4 +32,7 @@ app.use("/products",productsRouter)
 const indexRoute = require('./routes/index')
 app.use("/",indexRoute)
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -3,13 +3,15 @@ const config = require('config')
 
 const dbgr = require('debug')("development:mongoose")
 
-mongoose
-.connect(`${config.get("MONGODB_URI")}/SneakPeeks`)
-.then(function(){
-    dbgr("Connected")
-})
-.catch(function(err){
-    dbgr(err)
-})
+// mongoose
+// .connect(`${config.get("MONGODB_URI")}/SneakPeeks`)
+// .then(function(){
+//     dbgr("Connected")
+// })
+// .catch(function(err){
+//     dbgr(err)
+// })
+
+mongoose.connect(process.env.MONGODB_URI)
 
 module.exports = mongoose.connection
